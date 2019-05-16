562125517614ec09102faaa866bc6a62b044bc68
Konstantin Runduev <runduev@gmail.com>
2019-05-16 11:39:30 +0300

    signaling server

--- signalling/index.js
+++ signalling/index.js
@@ -1,20 +1,21 @@
 'use strict';
 
+
 var os = require('os');
 var nodeStatic = require('node-static');
-var https = require('http');
+var https = require('https');
 var socketIO = require('socket.io');
+
 var fs = require("fs");
 var options = {
-//	key: fs.readFileSync('key.pem'),
-//	cert: fs.readFileSync('cert.pem')
+	key: fs.readFileSync('key.pem'),
+	cert: fs.readFileSync('cert.pem')
 };
 
 var fileServer = new(nodeStatic.Server)();
 var app = https.createServer(options,function(req, res) {
   fileServer.serve(req, res);
-
-}).listen(1794);
+}).listen(8080);
 
 var io = socketIO.listen(app);
 io.sockets.on('connection', function(socket) {
@@ -24,32 +25,34 @@ io.sockets.on('connection', function(socket) {
     var array = ['Message from server:'];
     array.push.apply(array, arguments);
     socket.emit('log', array);
+    console.log(array);
   }
 
   socket.on('message', function(message) {
     log('Client said: ', message);
     // for a real app, would be room-only (not broadcast)
-    socket.broadcast.emit('message', message);  
-});
+    socket.broadcast.emit('message', message);
+  });
 
   socket.on('create or join', function(room) {
     log('Received request to create or join room ' + room);
 
-    var numClients = io.sockets.sockets.length;      
-log('Room ' + room + ' now has ' + numClients + ' client(s)');
+    var clientsInRoom = io.sockets.adapter.rooms[room];
+    var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
+    log('Room ' + room + ' now has ' + numClients + ' client(s)');
 
-    if (numClients === 1) {
+    if (numClients === 0) {
       socket.join(room);
       log('Client ID ' + socket.id + ' created room ' + room);
       socket.emit('created', room, socket.id);
 
-    } else if (numClients === 2) {
+    } else if (numClients === 1) {
       log('Client ID ' + socket.id + ' joined room ' + room);
       io.sockets.in(room).emit('join', room);
       socket.join(room);
       socket.emit('joined', room, socket.id);
       io.sockets.in(room).emit('ready');
-    } else { // max 5 clients
+    } else { // max two clients
       socket.emit('full', room);
     }
   });
@@ -64,10 +67,5 @@ log('Room ' + room + ' now has ' + numClients + ' client(s)');
       });
     }
   });
-
-  socket.on('bye', function(){
-    console.log('received bye');
-     
-});
 
 });
